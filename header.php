113488f2c383009f98b28a16dde90b8ca9680ecf
Mavericks(Bishal) <mavericksthinker@gmail.com>
2020-11-23 22:13:17 +0700

    Added gtag and minified thank you popup

--- header.php
+++ header.php
@@ -40,6 +40,15 @@ $container = get_theme_mod( 'understrap_container_type' );
           href="<?php echo get_template_directory_uri(); ?>/img/miscellaneous/charla-fb-cover.jpg"
     />
 	<?php wp_head(); ?>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-180794612-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-180794612-1');
+    </script>
 </head>
 
 <body <?php body_class(); ?> <?php understrap_body_attributes(); ?>>
